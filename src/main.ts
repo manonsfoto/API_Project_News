8adbcd48eef17e98f5822dcfcf7fb70477ae64d9
Khrystyna Barshchevska-Kostrova <barshchevskak@gmail.com>
2024-11-12 14:06:32 +0100

    const in ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
 import "./style.css";
+import { INews, IArticles, ISource } from "./interfaces/INews";
+
+const inputText = document.getElementById('inputText') as HTMLInputElement;
+const selectLang = document.getElementById('selectLang') as HTMLSelectElement;
+const selectSort = document.getElementById('selectSort') as HTMLSelectElement;
