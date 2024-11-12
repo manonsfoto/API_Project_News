a7430b7152b5a54d60c523200af3a8bba56e616e
Khrystyna Barshchevska-Kostrova <barshchevskak@gmail.com>
2024-11-12 14:43:27 +0100

    showCards

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import "./style.css";
+import { INews, IArticles, ISource } from "./interfaces/INews";
+
+const BASE_URL = `https://newsapi.org/v2/everything?`;
+const EVERYTHING_URL = `${BASE_URL}domains=wsj.com&apiKey=7db2c104c64a4d9f99dde5456344d6b0`;
 
 const inputText = document.getElementById('inputText') as HTMLInputElement;
 const selectLang = document.getElementById('selectLang') as HTMLSelectElement;
@@ -6,6 +10,15 @@ const selectSort = document.getElementById('selectSort') as HTMLSelectElement;
 const btn = document.querySelector('button') as HTMLButtonElement;
 const cardsWrapper = document.getElementById('cardsWrapper') as HTMLDivElement;
 
-function showList() {}
+function showCards(cards: INews[]) {
+    cardsWrapper.innerHTML = "";
+
+    cards.forEach((card: INews) => {
+        console.log(card);
+        cardsWrapper.appendChild(createCardElement(card))
+    })
+}
+
+function createCardElement(card: INews): HTMLElement{}
 
 function fetchList() {}
