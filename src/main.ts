1d629e2dd08325f6a63d4c240b56619940563162
Khrystyna Barshchevska-Kostrova <barshchevskak@gmail.com>
2024-11-12 17:57:58 +0100

    etwas geloescht

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./style.css";
-import { INews, IArticles, ISource } from "./interfaces/INews";
+import { INews, IArticles } from "./interfaces/INews";
 
 const inputText = document.getElementById("inputText") as HTMLInputElement;
 const selectLang = document.getElementById("selectLang") as HTMLSelectElement;
